e5c6241e3019daeb61b0710839470015fe485976
Tomdok <tomdokx@gmail.com>
2023-02-21 09:24:19 +0100

    Initial commit

---
+++ src/test/java/NNPIA/cv01/Cv01ApplicationTests.java
@@ -0,0 +1,13 @@
+package NNPIA.cv01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cv01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
